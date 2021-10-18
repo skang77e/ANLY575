3530580ab94785b0fd4f5f295a95c6c1da496a31
Jon Kang <jk2060@georgetown.edu>
2021-10-17 23:47:42 -0400

    add submission and update styles

--- data-site/submissions.content.php
+++ data-site/submissions.content.php
@@ -1,26 +1,33 @@
 <?php
 $db = new Database();
 
-$sql = '
-SELECT 
-	`s`.`id`,
-	`s`.`user_id`,
-	`u`.`firstname`,
-	`u`.`lastname`,
-	`s`.`assignment_id`,
-	`a`.`name`,
-	`s`.`datetime`,
-	`s`.`grade`
-FROM `submissions` AS `s`
-LEFT JOIN `users` AS `u`
-	ON `s`.`user_id` = `u`.`id`
-LEFT JOIN `assignments` AS `a`
-	ON `s`.`assignment_id` = `a`.`id`
-';
+$sql = 'SELECT `id` FROM `submissions`;';
 
+$submissions = $db->object('Submission');
 
-$submissions = $db->object('Submission', $sql);
+$tableStart = "<table>\n<caption>Submission</caption>\n<tr>\n";
+$tableStart .= "<th scope=\"col\">ID</th>\n";
+$tableStart .= "<th scope=\"col\">User ID</th>\n";
+$tableStart .= "<th scope=\"col\">Assignment ID</th>\n";
+$tableStart .= "<th scope=\"col\">Datetime</th>\n";
+$tableStart .= "<th scope=\"col\">Grade</th>\n";
+$tableStart .= "</tr>\n";
 
-echo '<pre>';
-print_r($submissions);
-echo '</pre>';
+$tableEnd = "</table>\n";
+
+$tableData = '';
+foreach ($submissions as $submission) {
+	$tableData .= "<tr>\n";
+	$tableData .= "<td>{$submission->id}</td>\n";
+	$tableData .= "<td>{$submission->user_id}</td>\n";
+	$tableData .= "<td>{$submission->assignment_id}</td>\n";
+	$tableData .= "<td>{$submission->datetime}</td>\n";
+	$tableData .= "<td>{$submission->grade}</td>\n";
+	$tableData .= "<td><a href=\"submission-edit.php?id={$submission->id}\" class=\"icon-button\"><i class=\"fas fa-pencil-alt\" role=\"img\" aria-label=\"Edit\"></i></a> ";
+	$tableData .= "<a href=\"submission-delete.php?id={$submission->id}\" class=\"icon-button\"><i class=\"far fa-trash-alt\" role=\"img\" aria-label=\"Delete\"></i></a></td>\n";
+	$tableData .= "</tr>\n";
+}
+
+$addContent = "<p><a href=\"submission-add.php\" class=\"icon-button\"><i class=\"fas fa-plus-circle\"></i> Add submission</a></p>";
+
+echo $tableStart . $tableData . $tableEnd . $addContent;
